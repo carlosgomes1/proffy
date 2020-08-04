3d53a66117edd630ec6159c2d7dee1ece49c6e81
carlosgomes1 <48812516+carlosgomes1@users.noreply.github.com>
2020-08-04 20:20:11 -0300

    Component Header e estilização do searcher de proffys e react-router-dom

---
+++ frontend/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+const TeacherForm: React.FC = () => {
+  return (
+    <div className="container" id="page-teacher-form">
+      <PageHeader title="Que incrível que você quer dar aulas."></PageHeader>
+    </div>
+  );
+};
+
+export default TeacherForm;
